baee5e3781897eb1d322dca22c66afd709995668
Matheus Henrique <matheus.hpp135@gmail.com>
2022-03-12 23:30:59 -0300

    Show score

--- frontend/src/components/MovieStars/index.tsx
+++ frontend/src/components/MovieStars/index.tsx
@@ -4,14 +4,38 @@ import { ReactComponent as StarHalf } from "assets/img/star-half.svg";
 
 import "./styles.css";
 
-function MovieStars() {
+type Props = {
+  score: number;
+};
+
+// EX:
+// getFills(3.5) => [1, 1, 1, 0.5, 0]
+// getFills(4.1) => [1, 1, 1, 1, 0.5]
+function getFills(score: number) {
+  const fills = [0, 0, 0, 0, 0];
+
+  const integerPart = Math.floor(score);
+
+  for (let i = 0; i < integerPart; i++) {
+    fills[i] = 1;
+  }
+
+  const diff = score - integerPart;
+  if (diff > 0) {
+    fills[integerPart] = 0.5;
+  }
+
+  return fills;
+}
+
+function MovieStars({ score }: Props) {
   return (
     <div className="dsmovie-stars-container">
-      <StarFull />
-      <StarFull />
-      <StarFull />
-      <StarHalf />
-      <StarEmpty />
+      {getFills(score).map((fill) => {
+        if (fill === 1) return <StarFull />;
+        if (fill === 0.5) return <StarHalf />;
+        return <StarEmpty />;
+      })}
     </div>
   );
 }
