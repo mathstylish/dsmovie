bb547dde9ba4eb7c65bc65220dad1f6adac5e8d3
Matheus Henrique <matheus.hpp135@gmail.com>
2022-03-13 04:36:32 -0300

    Update requests

--- frontend/src/utils/requests.ts
+++ frontend/src/utils/requests.ts
@@ -1,4 +1,4 @@
-const HOST = "localhost" || "127.0.0.1";
+const HOST = "localhost";
 const PORT = 8080;
 const LOCALHOST = `http://${HOST}:${PORT}`;
 export const BASE_URL = process.env.REACT_APP_BACKEND_URL ?? LOCALHOST;
